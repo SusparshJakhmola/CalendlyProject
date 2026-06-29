8616cbcd51bebf4eb471a2a00f56fc838620c863
Susparsh Jakhmola <susparsh@gmail.com>
2026-06-30 00:13:19 +0530

    Created new user api

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import {Request, Response} from "express";
-import {findAllUsers as findAllUsersService,findById as findByIdService} from "../services/user.service.js";
+import {findAllUsers as findAllUsersService,findById as findByIdService,createUser as createUserService} from "../services/user.service.js";
 import { sendSuccess } from "../utils/api-response.js";
 
 export async function findAllUsers(_req:Request, res:Response){
@@ -14,5 +14,6 @@ export async function findById(req:Request, res:Response){
 }
 
 export async function createUser(_req:Request, res:Response){
-    res.json({});
+    const newUser=await createUserService(_req.body);
+    sendSuccess(res, newUser,201,"User created successfully");
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,7 @@
-import {getAll, getById} from "../repositories/user.repository.js";
-
+import { CreateUserDto } from "../dtos/user.dto.js";
+import {create, getAll, getById} from "../repositories/user.repository.js";
+import {findByEmail} from "../repositories/user.repository.js";
+import { conflict } from "../utils/api-error.js";
 export async function findAllUsers(){
     const users=await getAll();
     return users;
@@ -10,5 +12,14 @@ export async function findById(id:number){
     if(!user){
         throw new Error("User not found");
     }
+    return user;
+}
+
+export async function createUser(data:CreateUserDto){
+    const existingUser=await findByEmail(data.email);
+    if(existingUser){
+        throw conflict("User already exists");
+    }
+    const user=await create(data);
     return user;
 }
