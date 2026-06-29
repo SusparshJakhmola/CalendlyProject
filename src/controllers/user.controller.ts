8fdeea3e84021f38615dcadeca0d1e24a1ed4d77
Susparsh Jakhmola <susparsh@gmail.com>
2026-06-29 23:28:07 +0530

    Added sendsuccess method successfully

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,16 @@
 import {Request, Response} from "express";
 import {findAllUsers as findAllUsersService,findById as findByIdService} from "../services/user.service.js";
+import { sendSuccess } from "../utils/api-response.js";
 
 export async function findAllUsers(_req:Request, res:Response){
     const response=await findAllUsersService();
-    res.json(response);
+    sendSuccess(res, response);
 }
 
 export async function findById(req:Request, res:Response){
     const {id}=req.params;
     const response=await findByIdService(Number(id));
-    res.json(response);
+    sendSuccess(res, response);
 }
 
 export async function createUser(_req:Request, res:Response){
